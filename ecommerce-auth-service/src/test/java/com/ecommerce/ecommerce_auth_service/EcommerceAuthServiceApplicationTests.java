928e699440189117e2008369c116b8c17ca0ce6f
gnyansoftnet <gnyan@softnet.co.in>
2026-03-09 22:47:06 +0530

    added intialize

---
+++ ecommerce-auth-service/src/test/java/com/ecommerce/ecommerce_auth_service/EcommerceAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
