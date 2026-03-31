3a737b5f292da17fac8497825101836f1950fcd7
gnyansoftnet <gnyan@softnet.co.in>
2026-03-31 22:37:27 +0530

    added

---
+++ ecommerce-discovery-server/src/test/java/com/ecommerce/ecommerce_discovery_server/EcommerceDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
