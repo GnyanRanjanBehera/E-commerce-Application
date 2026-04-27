cf2a1ebce1a51a69b5b6f9651b33c1f6a454f588
GnyanRanjanBehera <gnyanranjanbehera@gmail.com>
2026-04-27 22:40:06 +0530

    added

--- ecommerce-product-service/src/main/java/com/ecommerce/ecommerce_product_service/services/ProductService.java
+++ ecommerce-product-service/src/main/java/com/ecommerce/ecommerce_product_service/services/ProductService.java
@@ -1,6 +1,17 @@
 package com.ecommerce.ecommerce_product_service.services;
 
 public interface ProductService {
+    public ProductDto createProduct(ProductDto productDto);
+
+    public ProductDto getProduct(Long productId);
+
+    public ProductDto updateProduct(ProductDto productDto);
+
+    public void deleteProduct(Long id);
+
+    public Page
+
+
 
 
 }
